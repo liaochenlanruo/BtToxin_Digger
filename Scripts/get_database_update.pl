1e7b601703976c5b464e752341b2a63eb8bffdc3
liaochenlanruo <liaochenlanruo@webmail.hzau.edu.cn>
2021-12-27 19:37:51 +0800

    Update database to 20211227

--- Scripts/get_database_update.pl
+++ Scripts/get_database_update.pl
@@ -4,7 +4,7 @@ use warnings;
 
 my %hash;
 local $/ = ">";
-open IN, "bt_toxin20210802.fas" || die;# BtToxin_Digger database
+open IN, "bt_toxin20211011.fas" || die;# BtToxin_Digger database
 <IN>;
 while (<IN>) {
 	chomp;
